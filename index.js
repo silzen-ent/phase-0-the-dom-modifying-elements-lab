440dcc8c5059b3b80dd829f486ac5bb511ab0867
Alan Silva <silzen.ent@gmail.com>
2023-12-02 23:57:01 -0600

    Completed Creating & Inserting DOM Nodes Lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const mainElement = document.querySelector("main#main");
+mainElement.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id', 'victory');
+
+newHeader.textContent = "ALAN is the champion";
